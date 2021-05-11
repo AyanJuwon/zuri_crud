b6ba4bab96f488eee9a422af7dab236d4a613ba2
Ayanniran Oluwajuwon <oiayanniran@student.lautech.edu.ng>
2021-05-11 21:25:10 +0100

    starting all over

--- server.js
+++ server.js
@@ -4,8 +4,12 @@ const port = 4000;
 const username = "oluwajuwon";
 const password = "EmmAnUEl";
 app.use(express.json())
+app.use(express.urlencoded({ extended: false }));
 const mongoose = require('mongoose')
-const connectionString = `mongodb+srv://${username}:${password}@cluster0.vqzgm.mongodb.net/user-data?retryWrites=true&w=majority`;
+const connectionString = `mongodb+srv://zuri_data:${password}@cluster0.e7kyq.mongodb.net/sampleData?retryWrites=true&w=majority`
+// const connectionString = `mongodb+srv://${username}:${password}@cluster0.vqzgm.mongodb.net/user-data?retryWrites=true&w=majority`;
+// const connectionString = 'mongodb+srv://devayo:pttWYLfk3iFW2CM@test-cluster.r4iyn.mongodb.net/events?retryWrites=true&w=majority'
+
 
 mongoose.connect(connectionString, {
   useNewUrlParser: true,
@@ -30,44 +34,58 @@ app.get("/", (req, res) =>
 
 
 const userSchema = new mongoose.Schema({
-  firstname: String,
-  lastname: String,
-  country: String,
-  email: String,
+  firstname: 'string',
+  lastname: 'string',
+  country: 'string',
+  email: 'string',
 });
 
 
-const User = mongoose.model("User", userSchema);
+const User = mongoose.model("sampleUserData", userSchema);
 app.post(
   "/user-data",
   (req, res) => {
     const userData = req.body.userData;
     console.log(userData);
-    User.create({
-      firstname: req.body.title,
+    // User.create({
+    //   firstname: req.body.firstname,
+    //   lastname: req.body.lastname,
+    //   email: req.body.email,
+    //   country: req.body.country,
+    // })
+    const newData = new User({
+      firstname: req.body.firstname,
       lastname: req.body.lastname,
       email: req.body.email,
       country: req.body.country,
     });
+    newData.save(function (err) {
+      console.log('i am here')
+      if (err) return handleError(err);
+      // save
+      
+      return res.status(200).json({ message: "New Data created", newData });
+    });
   },
-  (err, newUser) => {
+  (err, newData) => {
     if (err) {
       return res.status(500).json({ message: err });
-    } else {
-      return res.status(200).json({ message: "New Data created", newUser });
+    } 
     }
-  }
+  
 );
 
 // GET request to retrieve single data
-app.get("/user-data/:id", (req, res) => {
-  User.findById(req.params.id, (err, userData) => {
+app.get("/getuser", (req, res) => {
+  console.log(req.params.id);
+  console.log(req.query.id);
+  User.findOne({_id: req.query.id}, (err, userData) => {
     if (err) {
       return res.status(500).json({ message: err });
     } else if (!userData) {
       return res.status(404).json({ message: "Data not found" });
     } else {
-      return res.status(200).json({ userData });
+      return res.status(200).json({ userData});
     }
   });
 });
@@ -76,6 +94,7 @@ app.get("/user-data/:id", (req, res) => {
 app.get("/user-data", (req, res) => {
   User.find({}, (err, userData) => {
     if (err) {
+      console.log(err)
       return res.status(500).json({ message: err });
     } else if (!userData) {
       return res.status(404).json({ message: "Data not found" });
